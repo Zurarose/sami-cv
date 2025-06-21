b6a14ed1bbae4773036c28f7641835e714260b79
Oleh <oleh@basal.dev>
2025-06-21 13:20:50 +0900

    🍱 refactor: adjust loader component positioning for improved layout

--- src/ui-kit/composite/loader/index.tsx
+++ src/ui-kit/composite/loader/index.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from 'lucide-react';
 
 export const Loader = () => {
   return (
-    <div className="relative mx-auto top-2/5 flex items-center justify-center">
+    <div className="relative mx-auto top-[250px] flex items-center justify-center">
       <Loader2 className="h-16 w-16 animate-spin" />
     </div>
   );
