1ac116fb372fd0105158e2f261a8adebeed0f7d6
Oleh <oleh@basal.dev>
2025-06-24 15:29:37 +0900

    🍱 refactor: streamline PDF generation process by optimizing code structure and removing redundant functions

---
+++ .puppeteerrc.mjs
@@ -0,0 +1,10 @@
+import { join } from 'path';
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+const config = {
+  cacheDirectory: join(process.cwd(), '.cache', 'puppeteer'),
+};
+
+export default config;
